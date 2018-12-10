57593fc7f9c43cf2d3ebe785823403dfd7b42919
Organt <vojtech.bludsky@gmail.com>
2018-12-10 15:26:03 +0100

    added gitignore etc.

---
+++ src/main/java/com/tribes_backend/tribes/TribesApplication.java
@@ -0,0 +1,12 @@
+package com.tribes_backend.tribes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TribesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TribesApplication.class, args);
+    }
+}
