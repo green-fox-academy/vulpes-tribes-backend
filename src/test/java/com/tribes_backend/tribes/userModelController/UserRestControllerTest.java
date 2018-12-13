d59169894aea1eee8fb02fddc374962d877dfd1a
Organt <vojtech.bludsky@gmail.com>
2018-12-13 16:16:56 +0100

    commit before merging with week-01

--- src/test/java/com/tribes_backend/tribes/userModelController/UserRestControllerTest.java
+++ src/test/java/com/tribes_backend/tribes/userModelController/UserRestControllerTest.java
@@ -1,10 +1,14 @@
 package com.tribes_backend.tribes.userModelController;
 
+import com.tribes_backend.tribes.userModel.TribesUser;
+import com.tribes_backend.tribes.userModel.UserModelHelpersMethods;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +21,8 @@ import static org.junit.Assert.*;
 public class UserRestControllerTest {
 
     private MockMvc mockMvc;
+    @Mock
+    private UserModelHelpersMethods userMethods;
     @InjectMocks
     private UserRestController userRestController;
 
@@ -31,12 +37,13 @@ public class UserRestControllerTest {
                 "  \"username\": \"adamgyulavari\",\n" +
                 "  \"password\": \"abc123\"\n" +
                 "}";
+//        Mockito.when(userMethods.usernameAlreadyTaken()).thenReturn(false)
         mockMvc.perform(MockMvcRequestBuilders.post("/register")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("adamgyulavari")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("abc123")));
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("adamgyulavari")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("abc123")));
     }
 
 }
