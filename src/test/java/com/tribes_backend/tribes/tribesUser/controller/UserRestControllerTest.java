d83f86989b0777dc2e70b3f01a146007f5fe9947
Organt <vojtech.bludsky@gmail.com>
2018-12-13 19:34:15 +0100

    first test for /register

--- src/test/java/com/tribes_backend/tribes/tribesUser/controller/UserRestControllerTest.java
+++ src/test/java/com/tribes_backend/tribes/tribesUser/controller/UserRestControllerTest.java
@@ -1,14 +1,20 @@
 package com.tribes_backend.tribes.tribesUser.controller;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.refEq;
+
+import com.tribes_backend.tribes.tribesUser.model.TribesUser;
 import com.tribes_backend.tribes.tribesUser.model.UserModelHelpersMethods;
 import com.tribes_backend.tribes.tribesUser.controller.UserRestController;
+import com.tribes_backend.tribes.tribesUser.service.UserCrudService;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,14 +25,18 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UserRestControllerTest {
 
-    private MockMvc mockMvc;
+
     @Mock
-    private UserModelHelpersMethods userMethods;
+    private UserCrudService userCrudService;
+    @Mock
+    private UserModelHelpersMethods userModelHelpersMethods;
     @InjectMocks
     private UserRestController userRestController;
 
+    private MockMvc mockMvc;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         mockMvc = MockMvcBuilders.standaloneSetup(userRestController).build();
     }
 
@@ -34,14 +44,19 @@ public class UserRestControllerTest {
     public void testRegisterUser() throws Exception {
         String json = "{\n" +
                 "  \"username\": \"adamgyulavari\",\n" +
-                "  \"password\": \"abc123\"\n" +
+                "  \"password\": \"12345678ab\"\n" +
                 "}";
+        TribesUser newUser = new TribesUser("adamgyulavari", "12345678ab");
+        Mockito.when(userCrudService.save(newUser)).thenReturn(true);
+        Mockito.when(userModelHelpersMethods.usernameAlreadyTaken(newUser)).thenReturn(false);
         mockMvc.perform(MockMvcRequestBuilders.post("/register")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.username", Matchers.is("adamgyulavari")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("abc123")));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password", Matchers.is("12345678ab")));
+                Mockito.verify(userCrudService).save(refEq(newUser));
+                Mockito.verify(userModelHelpersMethods).usernameAlreadyTaken(refEq(newUser));
     }
 
 }
