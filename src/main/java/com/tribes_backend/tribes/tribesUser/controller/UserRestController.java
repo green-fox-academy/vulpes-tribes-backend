d786d58a222612b3366676f6c17a7f01bb646d5e
Kopsova <jirina.kopsova@centrum.cz>
2018-12-14 11:33:22 +0100

    second

--- src/main/java/com/tribes_backend/tribes/tribesUser/controller/UserRestController.java
+++ src/main/java/com/tribes_backend/tribes/tribesUser/controller/UserRestController.java
@@ -1,19 +1,14 @@
 package com.tribes_backend.tribes.tribesUser.controller;
 
 
-import com.tribes_backend.tribes.tribesUser.model.UserModelHelpersMethods;
-import com.tribes_backend.tribes.tribesUser.model.TribesUser;
-import com.tribes_backend.tribes.tribesUser.repository.UserTRepository;
-
-
 import com.tribes_backend.tribes.tribesUser.errorService.ErrorMessagesMethods;
-
+import com.tribes_backend.tribes.tribesUser.model.TribesUser;
+import com.tribes_backend.tribes.tribesUser.model.UserModelHelpersMethods;
+import com.tribes_backend.tribes.tribesUser.repository.UserTRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.validation.annotation.Validated;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,8 +48,15 @@ public class UserRestController {
     public ResponseEntity loginUser(@RequestBody TribesUser tribesUser) {
 
         try {
-            return actorService.getActor(id);
-        } catch (ActorNotFoundException ex) {
+            if (userMethods.isValid(tribesUser)) {
+                if (userTRepository.findTribesUserByUsername(tribesUser.getUsername()).getPassword() ==
+                        tribesUser.getPassword()) {
+                    return new ResponseEntity(, HttpStatus.OK)
+                }
+
+
+                return actorService.getActor(id);
+            } catch(ActorNotFoundException ex){
             throw new ResponseStatusException(
                     HttpStatus.NOT_FOUND, "Actor Not Found", ex);
         }
