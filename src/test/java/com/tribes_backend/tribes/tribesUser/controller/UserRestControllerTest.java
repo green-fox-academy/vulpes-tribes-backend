fe80bd36aa37c150ff5462025f43cdfa52e06c58
Organt <vojtech.bludsky@gmail.com>
2018-12-14 10:44:45 +0100

    added two tests

--- src/test/java/com/tribes_backend/tribes/tribesUser/controller/UserRestControllerTest.java
+++ src/test/java/com/tribes_backend/tribes/tribesUser/controller/UserRestControllerTest.java
@@ -3,6 +3,8 @@ package com.tribes_backend.tribes.tribesUser.controller;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.refEq;
 
+import com.tribes_backend.tribes.tribesUser.errorService.ErrorMessagesMethods;
+import com.tribes_backend.tribes.tribesUser.errorService.ErrorResponseModel;
 import com.tribes_backend.tribes.tribesUser.model.TribesUser;
 import com.tribes_backend.tribes.tribesUser.model.UserModelHelpersMethods;
 import com.tribes_backend.tribes.tribesUser.controller.UserRestController;
@@ -19,6 +21,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -30,6 +33,10 @@ public class UserRestControllerTest {
     private UserCrudService userCrudService;
     @Mock
     private UserModelHelpersMethods userModelHelpersMethods;
+    @Mock
+    private ErrorMessagesMethods errorMessagesMethods;
+    @Mock
+    private TribesUser mockedUser;
     @InjectMocks
     private UserRestController userRestController;
 
@@ -41,7 +48,7 @@ public class UserRestControllerTest {
     }
 
     @Test
-    public void testRegisterUser() throws Exception {
+    public void testRegisterNewUser() throws Exception {
         String json = "{\n" +
                 "  \"username\": \"adamgyulavari\",\n" +
                 "  \"password\": \"12345678ab\"\n" +
@@ -59,4 +66,44 @@ public class UserRestControllerTest {
                 Mockito.verify(userModelHelpersMethods).usernameAlreadyTaken(refEq(newUser));
     }
 
+    @Test
+    public void testRegisterTakenUsername() throws Exception {
+        String json = "{\n" +
+                "  \"username\": \"adamgyulavari\",\n" +
+                "  \"password\": \"12345678ab\"\n" +
+                "}";
+        TribesUser newUser = new TribesUser("adamgyulavari", "12345678ab");
+        Mockito.when(userModelHelpersMethods.usernameAlreadyTaken(newUser)).thenReturn(true);
+        ErrorResponseModel toReturn = new ErrorResponseModel();
+        toReturn.setStatus("error");
+        toReturn.setErrorMessage("Username already taken, please choose another one.");
+        Mockito.when(errorMessagesMethods.usernameAlreadyTaken()).thenReturn(toReturn);
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(MockMvcResultMatchers.status().isConflict())
+                .andDo(MockMvcResultHandlers.print());
+        Mockito.verify(errorMessagesMethods).usernameAlreadyTaken();
+        Mockito.verify(userModelHelpersMethods).usernameAlreadyTaken(refEq(newUser));
+    }
+
+    @Test
+    public void testRegisterNullUsername() throws Exception {
+        String json = "{\n" +
+                "  \"username\": null,\n" +
+                "  \"password\": \"12345678ab\"\n" +
+                "}";
+        mockedUser = new TribesUser("adamgyulavari", "12345678ab");
+        Mockito.when(userModelHelpersMethods.usernameAlreadyTaken(mockedUser)).thenReturn(false);
+        Mockito.when(mockedUser.getUsername()).thenReturn(null);
+        ErrorResponseModel toReturn = new ErrorResponseModel();
+        toReturn.setStatus("error");
+        toReturn.setErrorMessage("Missing parameter(s): username");
+        Mockito.when(errorMessagesMethods.usernameAlreadyTaken()).thenReturn(toReturn);
+        mockMvc.perform(MockMvcRequestBuilders.post("/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(MockMvcResultMatchers.status().isConflict())
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
