bbee802063411dc7e19489ff12819f31bdef2f7e
Kopsova <jirina.kopsova@centrum.cz>
2018-12-18 13:35:25 +0100

    j

--- src/test/java/com/tribesbackend/tribes/TribesUserTest.java
+++ src/test/java/com/tribesbackend/tribes/TribesUserTest.java
@@ -10,11 +10,9 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class TribesUserTest {
-
     @Test
-    public void allUsersFailed(){
+    public void allUsersFailed() {
         List<TribesUser> userlist = new ArrayList<TribesUser>();
         userlist.add(new TribesUser(null, "123458"));
         userlist.add(new TribesUser("", "123458"));
@@ -28,32 +26,12 @@ public class TribesUserTest {
         for (int i = 0; i < userlist.size(); i++) {
             assertEquals(false, UserModelHelpersMethods.isValid(userlist.get(i)));
         }
-
-
     }
 
-
-
     @Test
-    public void userOK(){
+    public void userOK() {
         TribesUser tribesUser = new TribesUser("ddd", "1235");
         Assert.assertEquals(true, UserModelHelpersMethods.isValid(tribesUser));
     }
-    @Test
-    public void usernameMissing(){
-        TribesUser tribesUser = new TribesUser(null, "1235");
-        assertEquals(false, UserModelHelpersMethods.isValid(tribesUser));
-    }
-
-    @Test
-    public void passwordMissing(){
-        TribesUser tribesUser = new TribesUser("ddd", null);
-        assertEquals(false, UserModelHelpersMethods.isValid(tribesUser));
-    }
-    @Test
-    public void bothMissing(){
-        TribesUser tribesUser = new TribesUser(null, null);
-        assertEquals(false, UserModelHelpersMethods.isValid(tribesUser));
-    }
-
 }
+
