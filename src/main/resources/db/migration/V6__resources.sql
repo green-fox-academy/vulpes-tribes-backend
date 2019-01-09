4b7a8906fd699bab5413465415753acbe091ab6d
PeterXMR <peter.kozuch@zoho.com>
2019-01-09 14:26:53 +0100

    database migration is implemented - Table for Resource is created

---
+++ src/main/resources/db/migration/V6__resources.sql
@@ -0,0 +1,5 @@
+CREATE TABLE RESOURCES (ID INT AUTO_INCREMENT PRIMARY KEY,
+                        AMOUNT SMALLINT (10),
+                        TYPE VARCHAR(45),
+                        UPDATED_AT SMALLINT (10),
+                        KINGDOM VARCHAR (45);
