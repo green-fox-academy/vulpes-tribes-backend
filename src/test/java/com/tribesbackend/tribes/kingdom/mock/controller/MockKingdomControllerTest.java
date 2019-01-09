0b9b59c8264e2ce901bade9e656938dac04b77e6
Jiri Sousek <jirisousek@outlook.com>
2019-01-09 11:20:03 +0100

    MockKingdomTest resources json updated

--- src/test/java/com/tribesbackend/tribes/kingdom/mock/controller/MockKingdomControllerTest.java
+++ src/test/java/com/tribesbackend/tribes/kingdom/mock/controller/MockKingdomControllerTest.java
@@ -31,7 +31,7 @@ public class MockKingdomControllerTest {
 
     @Test
     public void testMockJson() throws Exception {
-        String json = "{\"name\":\"mockdom\",\"tribesUser\":{\"username\":\"mockUser\",\"password\":\"strongOne\"}}";
+        String json = "{\"name\":\"mockdom\",\"tribesUser\":{\"username\":\"mockUser\",\"password\":\"strongOne\"},\"resources\":null}";
         mockMvc.perform(MockMvcRequestBuilders.get("/mock/kingdom"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(json));
