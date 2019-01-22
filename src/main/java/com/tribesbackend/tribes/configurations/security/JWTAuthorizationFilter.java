ce1d7fd00114f7af5d378e5b023ad3c6f58fe41d
Kopsova <jirina.kopsova@centrum.cz>
2019-01-22 01:42:56 +0100

    JWT filter corrected

--- src/main/java/com/tribesbackend/tribes/configurations/security/JWTAuthorizationFilter.java
+++ src/main/java/com/tribesbackend/tribes/configurations/security/JWTAuthorizationFilter.java
@@ -1,16 +1,23 @@
 package com.tribesbackend.tribes.configurations.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import static com.tribesbackend.tribes.security.SecurityConstatns.SecurityConstants.HEADER_STRING;
-import static com.tribesbackend.tribes.security.SecurityConstatns.SecurityConstants.TOKEN_PREFIX;
-public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+import java.util.ArrayList;
 
-    public JWTAuthorizationFilter(AuthenticationManager authManager){
+import static com.tribesbackend.tribes.security.SecurityConstatns.SecurityConstants.HEADER_STRING;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+    public JWTAuthorizationFilter(AuthenticationManager authManager) {
         super(authManager);
     }
 
@@ -19,10 +26,32 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
                                     HttpServletResponse res,
                                     FilterChain chain) throws IOException, ServletException {
         String header = req.getHeader(HEADER_STRING);
+        System.out.println("Header is running");
+        System.out.println(header);
 
-        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+        if (header == null) {
             chain.doFilter(req, res);
             return;
         }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_STRING);
+        if (token != null) {
+            // parse the token.
+            String user = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()))
+                    .build()
+                    .verify(token)
+                    .getSubject();
+            System.out.println("USER IS:::::::::::::::::::::" + user);
+            if (user != null) {return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+        }
+        return null;
     }
 }
