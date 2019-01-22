17d57302fd40079dd11f41b1f0103d73dc1271d8
Organt <vojtech.bludsky@gmail.com>
2019-01-22 09:54:48 +0100

    repaired migrations

--- src/main/java/com/tribesbackend/tribes/services/controller/MyContoller.java
+++ src/main/java/com/tribesbackend/tribes/services/controller/MyContoller.java
@@ -2,7 +2,6 @@ package com.tribesbackend.tribes.tribesuser.controller;
 
 import java.security.Principal;
 
-import com.tribesbackend.tribes.tribeskingdom.model.Kingdom;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
