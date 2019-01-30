21c922cca478b8bec8ea50da3722feb988a0140a
Jiri Sousek <jirisousek@outlook.com>
2019-01-30 12:57:48 +0100

    list imported in troop endpoint

--- src/main/java/com/tribesbackend/tribes/controllers/TroopRestController.java
+++ src/main/java/com/tribesbackend/tribes/controllers/TroopRestController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TroopRestController extends BaseController {
     private TroopRepository troopRepository;
