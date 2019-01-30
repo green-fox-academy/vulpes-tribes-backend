277e6d5c684ff4ba514c665a45eae1df1c9c1afd
Adam Gyulavari <adam.gyulavari@lab.coop>
2019-01-30 16:45:58 +0100

    remove unnecessary authentication provider config and change passwordencoder to bcrypt

--- src/main/java/com/tribesbackend/tribes/security/SecurityConfig.java
+++ src/main/java/com/tribesbackend/tribes/security/SecurityConfig.java
@@ -4,14 +4,11 @@ import com.tribesbackend.tribes.services.userservice.MyUserTrDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -23,21 +20,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     ForbiddenExceptionHandler forbiddenExceptionHandler;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider
-                = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(myUserTrDetailsService);
-        authenticationProvider.setPasswordEncoder(encoder());
-        return authenticationProvider;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -54,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder encoder() {
+    public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder(11);
     }
 }
